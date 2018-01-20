9d9eb0d32ee0b36b121e756d01405a65af796565
automationgeek <jf.tremblay@gmail.com>
2018-01-19 23:58:27 -0500

    Update milight_poly.py

--- milight_poly.py
+++ milight_poly.py
@@ -31,7 +31,6 @@ class Controller(polyinterface.Controller):
         try:
             if 'host' in self.polyConfig['customParams']:
                 self.host = self.polyConfig['customParams']['host']
-                LOGGER.error(self.host)
             else:
                 self.host = ""
 
