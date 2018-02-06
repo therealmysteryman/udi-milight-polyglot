fb278820dcd44bba4bf6ca34f8b78310e7a66c2d
automationgeek <jf.tremblay@gmail.com>
2018-02-05 20:19:10 -0500

    Update commitToGit.sh

--- commitToGit.sh
+++ commitToGit.sh
@@ -1,3 +1,4 @@
 git add profile.zip
 git commit -m "generated profile zip [skip ci]"
-git push origin dev
+git remote add origin-pages https://$GITHUB_TOKEN@github.com/therealmysteryman/udi-milight-polyglot > /dev/null 2>&1
+git push --quiet --set-upstream origin-pages dev 
