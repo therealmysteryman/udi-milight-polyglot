f2c31a7b8c97884895d30b2bacfb83a5d54382dd
automationgeek <jf.tremblay@gmail.com>
2018-02-05 20:42:03 -0500

    Update commitToGit.sh

--- commitToGit.sh
+++ commitToGit.sh
@@ -1,8 +1,8 @@
-git checkout -b dev
-git status
 git add profile.zip
 git commit -m "generated profile zip [skip ci]"
-git status
+git checkout -b tmp_dev
+git checkout dev
+git merge my-tmp_dev-work
 git remote add origin-pages https://$GITHUB_TOKEN@github.com/therealmysteryman/udi-milight-polyglot 
 git status
 git push origin-pages dev 
