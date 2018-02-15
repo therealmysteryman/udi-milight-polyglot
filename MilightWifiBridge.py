5d67563474ddce15f78d8f5c98075e68e15f0118
automationgeek <jf.tremblay@gmail.com>
2018-02-15 18:13:13 -0500

    Update MilightWifiBridge.py

--- MilightWifiBridge.py
+++ MilightWifiBridge.py
@@ -313,11 +313,14 @@ class MilightWifiBridge:
     data_to_send = MilightWifiBridge.__START_SESSION_MSG
     # logging.debug("Sending frame '{}' to {}:{}".format(str(binascii.hexlify(data_to_send)),
     #                                                 str(self.__ip), str(self.__port)))
-    self.__sock.sendto(data_to_send, (self.__ip, self.__port))
-    response = MilightWifiBridge.__START_SESSION_RESPONSE(responseReceived=False, mac="", sessionId1=-1, sessionId2=-1)
+    try:
+      self.__sock.sendto(data_to_send, (self.__ip, self.__port))
+      response = MilightWifiBridge.__START_SESSION_RESPONSE(responseReceived=False, mac="", sessionId1=-1, sessionId2=-1)
 
-    # Receive start session response
-    data, addr = self.__sock.recvfrom(1024)
+      # Receive start session response
+      data, addr = self.__sock.recvfrom(1024)
+    except socket.timeout:
+      pass
     if len(data) == 22:
       # Parse valid start session response
       response = MilightWifiBridge.__START_SESSION_RESPONSE(responseReceived=True,
